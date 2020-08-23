521d09c755f1fb5eed7da2ed29d836b00f2dda0a
RongKeQian <qianer_lover@126.com>
2020-08-23 16:37:59 +0800

    上传

--- src/x.js
+++ src/x.js
@@ -1,2 +1,5 @@
 import "./x.css";
+import "./y.scss";
+import "./c.less";
+import "./z.styl";
 export default "xxx";
--- webpack.config.base.js
+++ webpack.config.base.js
@@ -19,6 +19,31 @@ module.exports = {
     }),
   ],
   module: {
-    rules: [],
+    rules: [
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.styl$/,
+        loader: ["style-loader", "css-loader", "stylus-loader"],
+      },
+      {
+        test: /\.less$/,
+        loader: ["style-loader", "css-loader", "less-loader"], // compiles Less to CSS
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader", // Creates `style` nodes from JS strings
+          "css-loader", // Translates CSS into CommonJS
+          // "sass-loader", // Compiles Sass to CSS
+          {
+            loader: "sass-loader",
+            options: { implementation: require("dart-sass") },
+          },
+        ],
+      },
+    ],
   },
 };
